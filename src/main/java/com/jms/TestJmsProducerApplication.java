635ed24af6b64500b9090d6bb8440e79e1f12bec
Mutakoduru Sainath <Mutakodurusainath@gmail.com>
2018-12-22 23:01:32 -0500

    first commit

---
+++ src/main/java/com/jms/TestJmsProducerApplication.java
@@ -0,0 +1,14 @@
+package com.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJmsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJmsProducerApplication.class, args);
+	}
+
+}
+
